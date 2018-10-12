bf4cd6bfc0786ed562d5bfa9efb4b8c1b48f9d05
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-12 10:38:06 +0530

    This script is modified,it avoids repeated terms
    in this script i removed the multiples of 15

--- MultiplesOf3And5.js
+++ MultiplesOf3And5.js
@@ -4,10 +4,18 @@ let limit=+process.argv[2]-1;
 let sum=0;
 let firstLimit=Math.floor(limit/firstMultiple);
 let secondLimit=Math.floor(limit/secondMultiple);
-for(let index=1;index<=firstLimit;index++){
-  sum+=(firstMultiple*index);
+let thirdLimit=Math.floor(limit/(firstMultiple*secondMultiple))
+const CalculateSum=function(){
+  for(let index=1;index<=firstLimit;index++){
+    sum+=(firstMultiple*index);
+  }
+  for(let index=1;index<=secondLimit;index++){
+    sum+=(secondMultiple*index);
+  }
+  for(let index=1;index<=thirdLimit;index++){
+    sum-=(firstMultiple*secondMultiple*index);
+  }
+  return sum;
 }
-for(let index=1;index<=secondLimit;index++){
-  sum+=(secondMultiple*index);
-}
-console.log(sum);
+exports.CalculateSum=CalculateSum;
+console.log(CalculateSum());
