955f11863745c29d5fde5314e99c5df7d1008955
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-12 12:14:00 +0530

    this script is modified and avoid global variables in functions
    this script prints the multiples of 3 or 5 below limit

--- MultiplesOf3And5.js
+++ MultiplesOf3And5.js
@@ -1,21 +1,22 @@
 let firstMultiple=3;
 let secondMultiple=5;
-let limit=+process.argv[2]-1;
+let thirdMultiple=(firstMultiple*secondMultiple);
+let limit=+process.argv[2];
 let sum=0;
-let firstLimit=Math.floor(limit/firstMultiple);
-let secondLimit=Math.floor(limit/secondMultiple);
-let thirdLimit=Math.floor(limit/(firstMultiple*secondMultiple))
-const CalculateSum=function(){
-  for(let index=1;index<=firstLimit;index++){
-    sum+=(firstMultiple*index);
+const CalculateSum=function(multiple,sum){
+  for(let index=multiple;index<limit;index+=multiple){
+    sum+=index;
   }
-  for(let index=1;index<=secondLimit;index++){
-    sum+=(secondMultiple*index);
-  }
-  for(let index=1;index<=thirdLimit;index++){
-    sum-=(firstMultiple*secondMultiple*index);
+  return sum;
+}
+const RemoveRepetition=function(multiple,sum){
+  for(let index=multiple;index<limit;index+=multiple){
+    sum-=index;
   }
   return sum;
 }
 exports.CalculateSum=CalculateSum;
-console.log(CalculateSum());
+firstMultipleSum=CalculateSum(firstMultiple,sum);
+secondMultipleSum=CalculateSum(secondMultiple,firstMultipleSum);
+removedRepetitions=RemoveRepetition(thirdMultiple,secondMultipleSum);
+console.log(removedRepetitions)
