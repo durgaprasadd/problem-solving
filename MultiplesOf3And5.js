9d07fd89c74e5f4f9121c7533ff9d39b3cebf5e2
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-15 10:23:38 +0530

    This script gives Multiples of 3 || 5 in 0.07s
    this script using formula

--- MultiplesOf3And5.js
+++ MultiplesOf3And5.js
@@ -1,18 +1,16 @@
 let firstMultiple=3;
 let secondMultiple=5;
 let thirdMultiple=(firstMultiple*secondMultiple);
-let limit=+process.argv[2];
+let limit=+process.argv[2]-1;
 let sum=0;
 const CalculateSum=function(multiple,sum){
-  for(let index=multiple;index<limit;index+=multiple){
-    sum+=index;
-  }
+  let range=Math.floor(limit/multiple);
+  sum+=multiple*(range/2)*(range+1);
   return sum;
 }
 const RemoveRepetition=function(multiple,sum){
-  for(let index=multiple;index<limit;index+=multiple){
-    sum-=index;
-  }
+  let range=Math.floor(limit/multiple);
+  sum-=multiple*(range/2)*(range+1);
   return sum;
 }
 exports.CalculateSum=CalculateSum;
